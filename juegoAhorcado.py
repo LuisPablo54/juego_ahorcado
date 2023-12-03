fd39a1f3bd54efa440de20060ce3e57eb82fa0c6
Luis Pablo Lopez Iracheta <luisirach54@gmail.com>
2023-12-02 20:36:43 -0600

    Menu de niveles

--- juegoAhorcado.py
+++ juegoAhorcado.py
@@ -10,24 +10,15 @@
 import random as rdn
 
 
-#Banco de palabras: son las listas que contienen las posibles palabras a a elegir
-AnimalesFacil = ["perro","vaca","delfin","caballo","gato","leon","panda","jirafa","pez","jaguar"]
+#Banco de palabras
 
-ColoresFacil = ["amarillo","verde","rojo","violeta","turquesa","marron","naranja","aguamarina","dorado","blanco",]
+Facil = ["amarillo","verde","rojo","violeta","turquesa","marron","naranja","aguamarina","dorado","blanco",]
 
-DeportesFacil = ["atletismo","esgrima","ciclismo","golf","natacion","tenis","baloncesto","boxeo","futbol","hockey"]
+Intermedio = ["turquia","suecia","argentina","ucrania","brasil","canada","dinamarca","francia","polonia"]
 
-PaisesIntermedio = ["turquia","qatar","suecia","argentina","ucrania","brasil","canada","dinamarca","francia","polonia"]
+Dificil = ["hortensia","azalea","pensamiento","orquidea","margarita","hibisco","bugambilia","lavanda","dalia","azucena"]
 
-ComidasIntermedio = ["guacamaya","torta","mole","pozole","palomitas","crepas","paella","poutine","tacos","tofu","pescado"]
 
-MarcasIntermedio = ["adidas","honda","sony","whatsapp","canon","pinterest","facebook","airbnb","starbucks","youtube"]
-
-FlorDificil = ["hortensia","azalea","pensamiento","orquidea","margarita","hibisco","bugambilia","lavanda","dalia","azucena"]
-
-CiudadesDificil = ["villahermosa","aguascalientes","poroy","batman","bankok","xbox","copenhague","melbourne","amsterdam"]
-
-InglesDificil = ["chemical","bought","nonplussed","whom","account","conversation","friend","group","indeed","international"]
 
 #Dibujo del ahorcado: una lista con 7 elementos que basicamente son dibujos echos con simbolos
 dibujos = ['''
@@ -87,3 +78,40 @@ dibujos = ['''
      |
     =========
     /       \ ''']
+
+#Menu de niveles par el usuario
+
+nombre = str(input("Nombre del jugador: "))
+print(f"\nPerfecto {nombre}, empezando el juego de arcado: \n")
+
+#Primero vamos a elegir con que plabara vamos a trabajar, para eso nos basamos en las preferencias del usuario
+#Lo de nuevo juego true y flase, solo es para lograr hacer un bucle, por si el usuario
+#no elige una opcion existente le vuelva a preguntar hasta que eliga una que si existe
+nuevojuego = True
+while nuevojuego == True:
+    nuevojuego = False
+    print("Primero, ¿Que dificultad deseas?")
+    print("\n Dificil \n Intermedio \n Facil \n")
+    dificultad = input()
+    #.Lower => es para pasar todas las letras de la variable a minusculas
+    dificultad = dificultad.lower()
+    #.strip => Elimina todos los espacios antes o despues en la cadena de texto
+    dificultad = dificultad.strip()
+    #aqui ya inician las deciciones, depende de la dificultat que desee el usuario
+    if dificultad == "dificil":
+        print("\nBuscas reto?, p\n")    
+        diccionario = Intermedio
+
+    elif dificultad == "intermedio":
+        print("\nBuena opcion, siempre en equilibrio \n")
+        diccionario = Intermedio
+         
+    elif dificultad == "facil":
+        print("\nVienes a relajarte, perfercto \n")
+        diccionario = Facil
+    #Si el usuario elige una categoria inexistente que "nuevojuego" valga otra vez true y con el "continue" se vuelva a ejecutar desde el primer bucle
+    else:
+        print("\n La opcion seleccionada no esta disponible, de favor corrige tu escritura \n")
+        nuevojuego = True
+        continue
+
